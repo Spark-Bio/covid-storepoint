3456ac8919d8ded3cf45a632a4e182eaed46fe88
Roderick Monje <rod@foveacentral.com>
2020-04-16 18:31:29 -0400

    test: covered :export_cac_as_storepoint

--- test/rake_test.rb
+++ test/rake_test.rb
@@ -12,6 +12,10 @@ class RakeTest < TestSitesTestCase
     end
   end
 
+  def test_export_cac_as_storepoint
+    Rake.application.invoke_task 'export_cac_as_storepoint'
+  end
+
   def test_update_storepoint
     Rake.application.invoke_task 'update_storepoint'
     assert_true FileUtils.compare_file('data/store_point.csv', 'test/fixtures/store_point.csv')
