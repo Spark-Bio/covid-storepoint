964770c135c44647c44991b95d417754422c21a2
Roderick Monje <rod@foveacentral.com>
2020-04-16 14:38:29 -0400

    test: covered compare_cac

--- test/rake_test.rb
+++ test/rake_test.rb
@@ -3,9 +3,14 @@
 require 'test_helper'
 
 class RakeTest < TestSitesTestCase
+  Rake.application.load_rakefile
+
+  def test_compare_cac
+    Rake.application.invoke_task 'compare_cac'
+    assert_true FileUtils.compare_file('data/cac_comparison.csv', 'test/fixtures/cac_comparison.csv')
+  end
+
   def test_update_storepoint
-    Rake.application.load_rakefile
-    File.delete('data/store_point.csv')
     Rake.application.invoke_task 'update_storepoint'
     assert_true FileUtils.compare_file('data/store_point.csv', 'test/fixtures/store_point.csv')
   end
