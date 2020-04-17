8e7320e6f80aa252c617e71ae07d4f0e06c576cf
Neil Berkman <neil@xuku.com>
2020-04-16 20:49:02 -0700

    Restore private declaration

--- lib/test_sites/cac.rb
+++ lib/test_sites/cac.rb
@@ -30,12 +30,14 @@ module TestSites
       @cac_data ||= Hashie::Array.new(JSON.parse(cac_raw_data))
     end
 
-    def cac_raw_data
-      Faraday.get('https://api.findcovidtesting.com/api/v1/location').body
-    end
+    private
 
     def local_data
       @local_data ||= CSV.read('test/fixtures/store_point.csv', headers: true)
+    end
+
+    def cac_raw_data
+      Faraday.get('https://api.findcovidtesting.com/api/v1/location').body
     end
 
     def cac_by_address
