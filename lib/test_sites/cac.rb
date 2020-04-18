4dd5420ce55ac7c400f68134590f8d03d16f2aab
Roderick Monje <rod@foveacentral.com>
2020-04-18 03:20:37 -0400

    refactor: CAC#cac_raw_data => CAC.cac_raw_data

--- lib/test_sites/cac.rb
+++ lib/test_sites/cac.rb
@@ -17,6 +17,10 @@ module TestSites
   class CAC
     SEPARATOR = ' - '
 
+    def self.cac_raw_data
+      Faraday.get('https://api.findcovidtesting.com/api/v1/location').body
+    end
+
     def dump_matches
       CSV.open(DataFile.path('cac_comparison.csv'),
                'w',
@@ -33,18 +37,14 @@ module TestSites
       cac_data.map { |cac_entry| cac_entry['location_hours_of_operation'] }
     end
 
-    def cac_data
-      @cac_data ||= Hashie::Array.new(JSON.parse(cac_raw_data))
-    end
-
     private
 
     def local_data
       @local_data ||= CSV.read('test/fixtures/store_point.csv', headers: true)
     end
 
-    def cac_raw_data
-      Faraday.get('https://api.findcovidtesting.com/api/v1/location').body
+    def cac_data
+      @cac_data ||= Hashie::Array.new(JSON.parse(CAC.cac_raw_data))
     end
 
     def cac_by_address
